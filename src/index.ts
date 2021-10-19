8d561be3f663b00003ad367bf03879f39ee12442
David Luzar <luzar.david@gmail.com>
2021-10-19 16:39:17 +0200

    default the CORS header to `webex.excalidraw.com`

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const io = socketIO(server, {
     const headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Access-Control-Allow-Origin":
-        (req.header && req.header.origin) || "https://excalidraw.com",
+        (req.header && req.header.origin) || "https://webex.excalidraw.com",
       "Access-Control-Allow-Credentials": true,
     };
     res.writeHead(200, headers);
